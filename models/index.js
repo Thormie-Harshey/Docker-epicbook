41b70a4e230ee99fd6f3e8ca89285f1cc1539998
Thormie-Harshey <ashayeadetomiwa77@gmail.com>
2026-04-23 07:30:41 +0100

    used variables for config.json and fixed index.js

--- models/index.js
+++ models/index.js
@@ -8,10 +8,21 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 let db = {};
 let sequelize;
+
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  // Logic for when a single connection string like JAWSDB_URL is provided
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // Modern DevOps logic: Use process.env variables mapped from your .env file
+  sequelize = new Sequelize(
+    process.env[config.username] || config.username,
+    process.env[config.password] || config.password,
+    process.env[config.database] || config.database,
+    {
+      host: process.env[config.host] || config.host,
+      dialect: config.dialect || "mysql"
+    }
+  );
 }
 
 fs.readdirSync(__dirname)
